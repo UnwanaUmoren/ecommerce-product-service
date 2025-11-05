b9d43afd0cbacda59e2a426a3a999ad96e5af3a5
Unwana Umoren <unwana@Unwanas-MacBook-Pro.local>
2025-11-05 19:04:05 +0000

    Adding in another endpoint

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return ("Hello there just playing with Flask")
+def homepage():
+    return ("Welcome to Wannie's E-commerce website")
+
+@app.route('/products')
+def pproducts():
+    return [
+        {'id': 1, 'name':'Sweatshirt', 'price':'£30.99'},
+        {'id': 2, 'name':'Jeans', 'price':'£49.99'},
+        {'id': 3, 'name':'Jacket', 'price':'£89.99'}
+    ]
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
